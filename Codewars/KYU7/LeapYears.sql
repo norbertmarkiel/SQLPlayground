b7cfac6c5677821ed8ae9e3cf4b7d28e409e799a
Norbert <norbertmarkiel@gmail.com>
2024-06-13 11:04:43 +0200

    Added LeapYears

---
+++ Codewars/KYU7/LeapYears.sql
@@ -0,0 +1,23 @@
+﻿-- https://www.codewars.com/kata/526c7363236867513f0005ca
+-- Leap Years
+
+SELECT 
+    year, 
+    CASE 
+        WHEN year % 400 = 0 THEN true
+        WHEN year % 100 = 0 THEN false
+        WHEN year % 4 = 0 THEN true
+        ELSE false
+    END AS is_leap
+FROM 
+    years
+ORDER BY 
+    year;
+
+--Alternative
+
+--select
+--  year,
+--  (year % 4 = 0 and year % 100 <> 0 ) or ( year % 400 = 0 ) as is_leap  -- your code here
+--from years
+--order by year asc;      -- result has to be sorted by year, ascending
