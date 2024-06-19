153f63198188979e641f4db88924e47828169962
Norbert <norbertmarkiel@gmail.com>
2024-06-19 09:35:30 +0200

    Added RegisterForTheParty

---
+++ Codewars/KYU8/RegisterForTheParty.sql
@@ -0,0 +1,8 @@
+--https://www.codewars.com/kata/590cc86f7557c0494000007e/sql
+--Register for the party
+
+INSERT into participants (name, age, attending) values ('Joe', 30, true);
+SELECT * FROM participants;
+
+--INSERT INTO participants VALUES ('Doe', '22', true);
+--SELECT * FROM participants;
